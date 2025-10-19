6a79cb3f2fca8234b24058be7e46ecf24ce81c10
Dat Vu <vdquangdat@gmail.com>
2025-10-19 18:02:27 +0800

    chore: reenable yarn pnp

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: ['./src/__tests__/setup.ts'],
     include: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
     coverage: {
